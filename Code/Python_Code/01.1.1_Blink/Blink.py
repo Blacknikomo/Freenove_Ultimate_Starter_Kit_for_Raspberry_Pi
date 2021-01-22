610db991f4e6cc3078c1035ea28f943db7df0ea8
Denis Lesnik <denis.lesnik@jetbrains.com>
2021-01-22 23:22:39 +0100

    Blink

--- Code/Python_Code/01.1.1_Blink/Blink.py
+++ Code/Python_Code/01.1.1_Blink/Blink.py
@@ -8,12 +8,12 @@
 import RPi.GPIO as GPIO
 import time
 
-ledPin = 11    # define ledPin
+ledPin = 5    # define ledPin
 
 def setup():
     GPIO.setmode(GPIO.BOARD)       # use PHYSICAL GPIO Numbering
     GPIO.setup(ledPin, GPIO.OUT)   # set the ledPin to OUTPUT mode
-    GPIO.output(ledPin, GPIO.LOW)  # make ledPin output LOW level 
+    GPIO.output(ledPin, GPIO.LOW)  # make ledPin output LOW level
     print ('using pin%d'%ledPin)
 
 def loop():
